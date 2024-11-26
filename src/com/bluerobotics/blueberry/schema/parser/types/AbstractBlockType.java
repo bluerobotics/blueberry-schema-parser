e4d493a6f0585612b7c6479504b91fafc60f0af4
Kenneth MacCallum <145465500+kennethblue@users.noreply.github.com>
2024-11-25 17:17:49 -0800

    Adding type definitions

---
+++ src/com/bluerobotics/blueberry/schema/parser/types/AbstractBlockType.java
@@ -0,0 +1,53 @@
+/*
+Copyright (c) 2024  Blue Robotics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package com.bluerobotics.blueberry.schema.parser.types;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public abstract class AbstractBlockType implements Type {
+	private final ArrayList<ByteType> m_baseTypes = new ArrayList<ByteType>();
+	private int m_bitCount = 0;
+
+	
+	public void add(ByteType t) {
+		if(t != null) {
+			m_bitCount += t.getSize().getBitCount();
+			m_baseTypes.add(t);
+		}
+	}
+	/**
+	 * gets the total number of bits of all the base types in this block
+	 * @return
+	 */
+	public int getBitCount() {
+		return m_bitCount;
+	}
+	
+	
+
+
+
+
+}
